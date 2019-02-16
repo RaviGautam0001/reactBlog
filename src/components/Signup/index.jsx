5e1a807a20a6aa5f4d3bd1433918880e03ea7ee9
=gautamravi0001 <gautamravi0001@gmail.com>
2019-02-16 03:07:06 -0800

    add signup validation

---
+++ src/components/Signup/index.jsx
@@ -0,0 +1,109 @@
+import React, { Component } from 'react'
+import { validateAll } from 'indicative';
+
+export class Signup extends Component {
+
+  constructor(){
+      super();
+      this.state = {
+          name: '',
+          email: '',
+          password: '',
+          password_confirmation: '',
+          errors: {}
+      }
+  }  
+
+  handleInputChange = (event) => {
+      this.setState({[event.target.name]: event.target.value});
+  }
+
+  handleSubmit = (event) => {
+      event.preventDefault();
+      console.log('state', this.state);
+
+      //validating user Data
+
+      const data = this.state;
+      const rules = {
+          name: 'required|string',
+          email: 'required|email',
+          password: 'required|string|min:6|confirmed',
+      }
+
+      validateAll(data, rules)
+        .then(() => {
+            //register the user
+        })
+        .catch(errors => {
+            //show validation errors
+            const formattedErros = {};
+            errors.forEach(error => formattedErros[error.field] = error.message);
+
+            this.setState({ errors: formattedErros });
+        })
+  }
+
+  render() {
+    return (
+        <div className="mh-fullscreen bg-img center-vh p-20" style={{backgroundImage: 'url(assets/img/bg-girl.jpg)'}}>
+        <div className="card card-shadowed p-50 w-400 mb-0" style={{maxWidth: '100%'}}>
+            <h5 className="text-uppercase text-center">Register</h5>
+            <br />
+            <br />
+            <form className="form-type-material" onSubmit={this.handleSubmit}>
+                
+                <div className="form-group">
+                    <input 
+                    type="text" 
+                    name="name"
+                    className="form-control" 
+                    placeholder="Username" 
+                    onChange={this.handleInputChange}
+                    />
+                </div>
+                
+                <div className="form-group">
+                    <input 
+                    type="text"
+                    name="email" 
+                    className="form-control" 
+                    placeholder="Email address" 
+                    onChange={this.handleInputChange}
+                    />
+                </div>
+
+                <div className="form-group">
+                    <input 
+                    type="password" 
+                    name="password"
+                    className="form-control" 
+                    placeholder="Password" 
+                    onChange={this.handleInputChange}
+                    />
+                </div>
+
+                <div className="form-group">
+                    <input 
+                    type="password" 
+                    name="password_confirmation"
+                    className="form-control" 
+                    placeholder="Password (confirm)" 
+                    onChange={this.handleInputChange}
+                    />
+                </div>
+
+                <br />
+                <button className="btn btn-bold btn-block btn-primary" type="submit">Register</button>
+            </form>
+            <hr className="w-30" />
+            <p className="text-center text-muted fs-13 mt-20">Already have an account?
+                <a href="login.html">Sign in</a>
+            </p>
+        </div>
+    </div>
+    )
+  }
+}
+
+export default Signup
--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Route, Link, withRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import Navbar from './components/Navbar';
+import Welcome from './components/Welcome';
+import Footer from './components/Footer';
+import CreateArticle from './components/CreateArticle';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import SingleArticle from './components/SingleArticle';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const Main = withRouter(({ location }) => {
+      
+  return (
+
+        <div>       
+            { location.pathname !== '/login' && location.pathname !== '/signup' &&  <Navbar />  }
+            <Route exact path="/" component={Welcome} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/article/:slug" component={SingleArticle} />
+            <Route exact path="/articles/create" component={CreateArticle} />
+            { location.pathname !== '/login' && location.pathname !== '/signup' && <Footer /> }
+        </div>
+
+  );
+})
+
+
+ReactDOM.render(
+     <BrowserRouter>
+      
+      <Main />
+      
+     </BrowserRouter>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
